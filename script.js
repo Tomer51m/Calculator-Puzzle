3c7c988b08a12a3207a0bdcc87d4256590a70103
Tomer Matmon <tomer.matmon@gmail.com>
2019-06-17 14:31:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,136 @@
+// global variables
+let calculate = '';
+let draggedElement;
+let draggedElementParent;
+let currentElement;
+
+//dom element
+const resultField = document.querySelector('.result');
+const startBtn = document.querySelector('.start');
+const numberButtons = document.querySelectorAll('.number-container');
+const specialButtons = document.querySelectorAll('.special-container');
+const equelButton = document.querySelector('.equel');
+const acButton = document.querySelector('.ac');
+
+shuffleCalculator()
+//shuffle numbers
+function shuffleCalculator() {
+    let numbersArrayInner = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let numbersArrayOuter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let i = 0
+    while (numbersArrayOuter != 0) {
+        const randomOuter = Math.floor(Math.random() * numbersArrayOuter.length);
+        const randomInner = Math.floor(Math.random() * numbersArrayInner.length);
+        const randomNumberInner = numbersArrayInner.splice(randomOuter, 1);
+        const randomNumberOuter = numbersArrayOuter.splice(randomInner, 1);
+        numberButtons[i].attributes[1].value = randomNumberOuter;
+        numberButtons[i].children[0].innerText = randomNumberInner;
+        i += 1
+    }
+}
+
+function checkIfWon() {
+    const currentButtonContainer = [];
+    const currentButtonValue = [];
+    
+    for (let i = 0; i < 10; i += 1) {
+        currentButtonContainer.push(numberButtons[i].attributes[1].value)
+        currentButtonValue.push(numberButtons[i].children[0].innerText) 
+    }
+
+    if(JSON.stringify(currentButtonContainer) === JSON.stringify(currentButtonValue) ) {
+        for(button of numberButtons) {
+            button.children[0].classList.add('won');                
+        }
+    }
+}
+
+//number buttons event listeners
+for (const button of numberButtons ) {
+    button.addEventListener('click', (event) => {        
+        if(resultField.innerText === '0') {
+            resultField.innerText = '';
+        }
+        
+        resultField.innerText += event.target.innerText;
+        calculate += event.target.parentElement.attributes[1].value;
+        button.classList.add('disable');
+        console.log(calculate);
+    })
+}
+
+//special buttons event listeners
+for (const special of specialButtons) {
+    special.addEventListener('click', (event) => {
+        if(calculate === '') {
+            return
+        }
+        calculate += event.target.parentElement.attributes[1].value;
+        resultField.innerText += event.target.parentElement.attributes[1].value;
+        // console.log(calculate);
+    })
+}
+
+equelButton.addEventListener('click', () => {
+    const result = eval(calculate) || '0';
+    resultField.innerText = result;
+    for (const button of numberButtons ) {
+        button.classList.remove('disable');
+    }
+})
+
+acButton.addEventListener('click', () => {
+    resultField.innerText = '0';
+    calculate = '';
+    for (const button of numberButtons ) {
+        button.classList.remove('disable');
+    }
+})
+
+startBtn.addEventListener('click', (event) => {
+    for(button of numberButtons) {
+        button.children[0].classList.remove('won');   
+        button.classList.remove('disable')             
+    }
+    resultField.innerText = '0';
+    calculate = '';
+    shuffleCalculator();
+})
+
+//drag and drop functionality
+for(const button of numberButtons) {
+    button.addEventListener('dragstart', () => {
+        draggedElement = event.target;
+        draggedElementParent = event.target.parentElement;
+    })
+
+    button.addEventListener('dragenter', (event) => {
+        if(event.target.className === 'button') {
+            event.target.style.opacity = .5;
+        }
+    })
+
+    button.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    })
+
+    button.addEventListener('dragleave', (event) => {
+        if(event.target.className === 'button') {
+            event.target.style.opacity = '';
+        }
+    })
+
+    button.addEventListener('drop', (event) => {
+        event.preventDefault();
+        
+        if(event.target.parentElement.className === 'botton-container number-container') {
+            currentElement = event.target;
+            event.target.style.opacity = '';
+            currentElement.parentElement.replaceChild(draggedElement, currentElement);
+            draggedElementParent.appendChild(currentElement);
+        }
+        resultField.innerText = '0';
+        calculate = '';
+        checkIfWon()
+    })
+}
